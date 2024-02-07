82498d2964c0bbba948454b7ea47e8f36aa91549
BradleyGilden <bradleygilden@gmail.com>
2024-02-07 15:43:28 +0200

    custom filters

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -6,9 +6,10 @@ Date: 07-02-2024
 """
 import re
 from typing import List
+import logging
 
 
-def filter_datum(fields: List, redaction: str, message: str,
+def filter_datum(fields: List[str], redaction: str, message: str,
                  separator: str) -> str:
     """obfuscates log messages depending given paramenters"""
     filter = message
@@ -16,3 +17,21 @@ def filter_datum(fields: List, redaction: str, message: str,
         filter = re.sub(r'(?<={fld}\=)[^{sp}]+(?={sp})'
                         .format(sp=separator, fld=field), redaction, filter)
     return filter
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """formats the message of the log"""
+        return filter_datum(self.fields, self.REDACTION,
+                            super().format(record), self.SEPARATOR)
--- 0x00-personal_data/tests/1-main.py
+++ 0x00-personal_data/tests/1-main.py
@@ -1,11 +1,22 @@
 #!/usr/bin/env python3
 
 """
-<module docstring>
+Testing filter_datum
 
 Author: Bradley Dillion Gilden
 Date: 07-02-2024
 """
+import os
+import sys
+import logging
+
+sys.path.append(os.path.abspath(os.path.abspath(__file__) + "/../../"))
+
+from filtered_logger import RedactingFormatter
 
 
 if __name__ == '__main__':
+    message = "name=Bob;email=bob@dylan.com;ssn=000-123-0000;password=bobby2019;"
+    log_record = logging.LogRecord("my_logger", logging.INFO, None, None, message, None, None)
+    formatter = RedactingFormatter(fields=("email", "ssn", "password"))
+    print(formatter.format(log_record))
