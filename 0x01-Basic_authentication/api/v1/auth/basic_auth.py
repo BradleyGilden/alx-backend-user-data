6b5ac467117a24be3855a841704cafdcb21102e6
BradleyGilden <bradleygilden@gmail.com>
2024-02-12 20:07:31 +0200

    basic authorization

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -6,11 +6,13 @@ Handles base64 strings in Authorization header
 Author: Bradley Dillion Gilden
 Date: 12-02-2024
 """
+from api.v1.auth.auth import Auth
+from models.user import User
 import base64
 from typing import TypeVar
 
 
-class BasicAuth:
+class BasicAuth(Auth):
     """ Handles base64 strings in Authorization header
     """
     def extract_base64_authorization_header(
@@ -62,3 +64,28 @@ class BasicAuth:
             return None
         elif (user_pwd is None or type(user_pwd) is not str):
             return None
+        try:
+            users = User.search({'email': user_email})
+            if not users or users == []:
+                return None
+            for u in users:
+                if u.is_valid_password(user_pwd):
+                    return u
+            return None
+        except Exception:
+            return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ get the current user
+        """
+
+        Auth_header = self.authorization_header(request)
+        if Auth_header is not None:
+            token = self.extract_base64_authorization_header(Auth_header)
+            if token is not None:
+                decoded = self.decode_base64_authorization_header(token)
+                if decoded is not None:
+                    email, pword = self.extract_user_credentials(decoded)
+                    if email is not None:
+                        return self.user_object_from_credentials(email, pword)
+        return
--- 0x01-Basic_authentication/tests/5-main.py
+++ 0x01-Basic_authentication/tests/5-main.py
@@ -1,11 +1,39 @@
 #!/usr/bin/env python3
-
+""" Main 5
 """
-<module docstring>
+import sys
+import os
+import uuid
+sys.path.append(os.path.dirname(os.path.abspath(__name__)))
+from models.user import User
+from api.v1.auth.basic_auth import BasicAuth
 
-Author: Bradley Dillion Gilden
-Date: 12-02-2024
-"""
+""" Create a user test """
+user_email = str(uuid.uuid4())
+user_clear_pwd = str(uuid.uuid4())
+user = User()
+user.email = user_email
+user.first_name = "Bob"
+user.last_name = "Dylan"
+user.password = user_clear_pwd
+print("New user: {}".format(user.display_name()))
+user.save()
 
+""" Retreive this user via the class BasicAuth """
 
-if __name__ == '__main__':
+a = BasicAuth()
+
+u = a.user_object_from_credentials(None, None)
+print(u.display_name() if u is not None else "None")
+
+u = a.user_object_from_credentials(89, 98)
+print(u.display_name() if u is not None else "None")
+
+u = a.user_object_from_credentials("email@notfound.com", "pwd")
+print(u.display_name() if u is not None else "None")
+
+u = a.user_object_from_credentials(user_email, "pwd")
+print(u.display_name() if u is not None else "None")
+
+u = a.user_object_from_credentials(user_email, user_clear_pwd)
+print(u.display_name() if u is not None else "None")
