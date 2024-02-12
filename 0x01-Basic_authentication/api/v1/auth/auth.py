d1345f0cda0969c5203640ef136ea99cac43b62e
BradleyGilden <bradleygilden@gmail.com>
2024-02-12 13:43:25 +0200

    testing

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -7,6 +7,20 @@ Author: Bradley Dillion Gilden
 Date: 12-02-2024
 """
 from flask import request
+from typing import List, TypeVar
 
 
-if __name__ == '__main__':
+class Auth:
+    """ a class to manage the API authentication
+    """
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """unknown functionality"""
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """the Flask request object"""
+        return request
+
+    def current_user(self, request=None) -> TypeVar('User'):  # type: ignore
+        """the Flask request object"""
+        return request
