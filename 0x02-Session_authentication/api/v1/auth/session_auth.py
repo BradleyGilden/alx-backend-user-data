63b913d0bbf2205e33ebe75b66daf8f939058cfa
BradleyGilden <bradleygilden@gmail.com>
2024-02-14 13:15:15 +0200

    creating session class

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+"""
+Handles session authorization and authentication
+
+Author: Bradley Dillion Gilden
+Date: 14-02-2024
+"""
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """Handles session authorization and authentication"""
