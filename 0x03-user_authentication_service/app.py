facb920195336126bbe42e6d660552e3c5b8f05e
BradleyGilden <bradleygilden@gmail.com>
2024-02-20 11:03:49 +0200

    create login session

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -6,7 +6,7 @@ This module contains the routes to the flask application
 Author: Bradley Dillion Gilden
 Date: 19-02-2024
 """
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, make_response, abort
 from auth import Auth
 
 AUTH = Auth()
@@ -31,6 +31,21 @@ def reg_user() -> str:
         return jsonify({"email": user.email, "message": "user created"})
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
+
+
+@app.route("/sessions", methods=["POST"], strict_slashes=False)
+def login() -> str:
+    """ creates a login session for a user if login details are correct
+    """
+    fields = request.form
+    if (AUTH.valid_login(fields.get("email"), fields.get("password"))):
+        email = fields["email"]
+        sid = AUTH.create_session(email)
+        resp = make_response(jsonify({"email": email, "message": "logged in"}))
+        resp.set_cookie("session_id", sid)
+        return resp
+    else:
+        abort(401)
 
 
 if __name__ == "__main__":
